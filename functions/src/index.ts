75130f308bedc84ee52280d1340b5fcd2aecf1b7
Jordan Khalil <jordankhalil0@gmail.com>
2023-04-06 14:46:23 -0400

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import itemsRouter from "./routes/itemsRouter";
 import accountRouter from "./routes/accountRouter";
 import dotenv from "dotenv";
-
+dotenv.config();
 // defines Express server that is used to handle our HTTP requests.
 
 const app = express();
@@ -13,6 +13,5 @@ app.use(cors());
 app.use(express.json());
 app.use("/account", accountRouter);
 app.use("/api/shopping-cart", itemsRouter);
-dotenv.config();
 
 export const api = functions.https.onRequest(app);
